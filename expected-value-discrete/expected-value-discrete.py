67cda3a36b5e34fdee35f05a7a3451fd337d8135
Anes <101956173+AnesABDOU@users.noreply.github.com>
2026-09-18 18:58:10 +0200

    Solve expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def expected_value_discrete(x: list, p: list) -> float:
+    """
+    Returns the expected value as a Python float.
+    """
+    x = np.asarray(x)
+    p = np.asarray(p)
+
+    return float(np.sum(x * p))
+    pass
